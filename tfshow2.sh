f6553864b4de6c6b9b83ae050acccfd0adac482c
aron-francis <aron.francis@yahoo.com>
2024-09-10 11:50:52 +0200

    1

--- tfshow2.sh
+++ tfshow2.sh
@@ -7,60 +7,91 @@ print_header() {
     echo "----------------------------------------"
 }
 
+# Function to print resource count
+print_count() {
+    echo "----------------------------------------"
+    echo "Total resources: $1"
+    echo "----------------------------------------"
+}
+
+# Function to count resources
+count_resources() {
+    echo "$1" | sed '1,2d' | grep -v '^|--' | grep -v '^\+--' | grep -v '^$' | wc -l
+}
+
+# Variable to keep track of total resources
+total_resources=0
+: <<'END_COMMENT'
 # VPC
 print_header "VPC(s)"
-aws ec2 describe-vpcs --query 'Vpcs[*].[VpcId,CidrBlock,Tags[?Key==`Name`].Value|[0]]' --output table
+vpc_output=$(aws ec2 describe-vpcs --query 'Vpcs[*].[VpcId,CidrBlock,Tags[?Key==`Name`].Value|[0]]' --output table)
+echo "$vpc_output"
+vpc_count=$(count_resources "$vpc_output")
+print_count $vpc_count
+total_resources=$((total_resources + vpc_count))
 
 sleep 2
 
 # Subnets
 print_header "Subnets"
-aws ec2 describe-subnets --query 'Subnets[*].[SubnetId,CidrBlock,AvailabilityZone,Tags[?Key==`Name`].Value|[0]]' --output table
+subnet_output=$(aws ec2 describe-subnets --query 'Subnets[*].[SubnetId,CidrBlock,AvailabilityZone,Tags[?Key==`Name`].Value|[0]]' --output table)
+echo "$subnet_output"
+subnet_count=$(count_resources "$subnet_output")
+print_count $subnet_count
+total_resources=$((total_resources + subnet_count))
 
 sleep 2
 
 # Internet Gateway
 print_header "Internet Gateway(s)"
-aws ec2 describe-internet-gateways --query 'InternetGateways[*].[InternetGatewayId,Attachments[0].VpcId]' --output table
+igw_output=$(aws ec2 describe-internet-gateways --query 'InternetGateways[*].[InternetGatewayId,Attachments[0].VpcId]' --output table)
+echo "$igw_output"
+igw_count=$(count_resources "$igw_output")
+print_count $igw_count
+total_resources=$((total_resources + igw_count))
 
 sleep 2
 
 # Route Table
 print_header "Route Table(s)"
-aws ec2 describe-route-tables --query 'RouteTables[*].[RouteTableId,VpcId,Tags[?Key==`Name`].Value|[0]]' --output table
+rt_output=$(aws ec2 describe-route-tables --query 'RouteTables[*].[RouteTableId,VpcId,Tags[?Key==`Name`].Value|[0]]' --output table)
+echo "$rt_output"
+rt_count=$(count_resources "$rt_output")
+print_count $rt_count
+total_resources=$((total_resources + rt_count))
 
 sleep 2
+END_COMMENT
 
-# EC2 Instance
-print_header "EC2 Instance(s)"
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,InstanceType,State.Name,PublicIpAddress,Tags[?Key==`Name`].Value|[0]]' --output table
+# EC2 Instance - Excluding Terminated
+print_header "EC2 Instance(s) - Excluding Terminated"
+ec2_output=$(aws ec2 describe-instances \
+    --filters "Name=instance-state-name,Values=pending,running,stopping,stopped" \
+    --query 'Reservations[*].Instances[*].[InstanceId,InstanceType,State.Name,PublicIpAddress,Tags[?Key==`Name`].Value|[0]]' \
+    --output table)
+echo "$ec2_output"
+ec2_count=$(count_resources "$ec2_output")
+print_count $ec2_count
+total_resources=$((total_resources + ec2_count))
 
 sleep 2
 
 # Lambda Functions
 print_header "Lambda Function(s)"
-aws lambda list-functions --query 'Functions[*].[FunctionName,Runtime,Handler]' --output table
+lambda_output=$(aws lambda list-functions --query 'Functions[*].[FunctionName,Runtime,Handler]' --output table)
+echo "$lambda_output"
+lambda_count=$(count_resources "$lambda_output")
+print_count $lambda_count
+total_resources=$((total_resources + lambda_count))
 
 sleep 2
 
 # SNS Topics
 print_header "SNS Topic(s)"
-aws sns list-topics --query 'Topics[*].[TopicArn]' --output table
+sns_output=$(aws sns list-topics --query 'Topics[*].[TopicArn]' --output table)
+echo "$sns_output"
+sns_count=$(count_resources "$sns_output")
+print_count $sns_count
+total_resources=$((total_resources + sns_count))
 
 sleep 2
-
-# CloudWatch Event Rules
-print_header "CloudWatch Event Rule(s)"
-aws events list-rules --query 'Rules[*].[Name,ScheduleExpression]' --output table
-
-sleep 2
-
-
-# RDS Instance
-print_header "RDS Instance(s)"
-aws rds describe-db-instances --query 'DBInstances[*].[DBInstanceIdentifier,Engine,DBInstanceClass,DBInstanceStatus,Endpoint.Address]' --output table || echo "No RDS instances found or an error occurred"
-
-# Add error checking
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to retrieve RDS instances. Check your AWS credentials and permissions."
-fi
